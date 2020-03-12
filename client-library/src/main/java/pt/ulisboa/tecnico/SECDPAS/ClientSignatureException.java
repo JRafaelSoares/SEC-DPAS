38c798049b3b57f24eb278ac4930aff1441dc087
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-03-12 15:46:55 +0000

    Added Signature Exception

---
+++ client-library/src/main/java/pt/ulisboa/tecnico/SECDPAS/ClientSignatureException.java
@@ -0,0 +1,8 @@
+package pt.ulisboa.tecnico.SECDPAS;
+
+public class ClientSignatureException extends Exception{
+
+    public ClientSignatureException() {super();}
+
+    public ClientSignatureException(String message) {super(message);}
+}
