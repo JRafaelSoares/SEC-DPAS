9038d06297f814727ab368f7ed7ae8e9efdd4fde
JoanaCoutinho <joana.coutinho@tecnico.ulisboa.pt>
2020-03-31 17:54:54 +0100

    merged

---
+++ client-library/src/main/java/pt/ulisboa/tecnico/SECDPAS/ComunicationException.java
@@ -0,0 +1,8 @@
+package pt.ulisboa.tecnico.SECDPAS;
+
+public class ComunicationException extends Exception{
+
+    public ComunicationException() {super();}
+
+    public ComunicationException(String message) {super(message);}
+}
