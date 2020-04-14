d7d11374bf51de6a35a79d7681ecb39e303f9926
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2020-04-14 15:11:51 +0100

    Added better debugging to server

--- dpas-server/src/main/java/pt/ulisboa/tecnico/SECDPAS/DPASServer.java
+++ dpas-server/src/main/java/pt/ulisboa/tecnico/SECDPAS/DPASServer.java
@@ -5,40 +5,14 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
-import java.security.cert.Certificate;
 
 
 public class DPASServer {
-
-	/*
-	//private String[] arguments;
-	private Socket clientSocket;
-	private ServerSocket serverSocket;
-	private INCSClient incsClient;
-	private Server server;
-	private int toggle;
-	private static String mode;
-	private static NonConfidentialMessageHandler nonConfidentialMessageHandler;
-	private String baseDir;
-	*/
-	public DPASServer() {
-		/*
-		clientSocket = clientSoc;
-		serverSocket = serverSoc;
-		this.incsClient = incsClient;
-		server = manServer;
-		toggle = tog;
-		this.baseDir = baseDir;
-		*/
-	}
 
 	public static void main(String[] args) throws Exception {
 
@@ -74,7 +48,7 @@ public class DPASServer {
 			Server server = ServerBuilder.forPort(portClient).addService(impl).build();
 			server.start();
 
-			System.out.println("[SERVER] Server started \n");
+			System.out.println(String.format("[SERVER] Server %d started in location localhost:%d \n", serverID, portClient));
 
 			// Do not exit the main thread. Wait until server is terminated.
 			server.awaitTermination();
